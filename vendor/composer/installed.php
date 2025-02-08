70be95922c94810701bb253fee76f2dbbdfb94ab
Christian Kloewer <kloewer@gmail.com>
2025-02-08 22:18:49 +0100

    Include vendor and plugin-update-checker

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'kloewer/aibotstudio',
+  ),
+  'versions' => 
+  array (
+    'kloewer/aibotstudio' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'yahnis-elsts/plugin-update-checker' => 
+    array (
+      'pretty_version' => 'v5.5',
+      'version' => '5.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '845d65da93bcff31649ede00d9d73b1beadbb7f0',
+    ),
+  ),
+);
